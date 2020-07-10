12b838cd2c66860e74a4d3ef3e331e583486bc61
Axel Obscura <axosar@gmail.com>
2020-07-10 10:20:03 -0500

    Updates

---
+++ precache-manifest.9dd4be15d1fb5164e948aea29b09d30a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ca93bfe046101bcb66f1d696c3df91",
+    "url": "/dashboard-gestell/index.html"
+  },
+  {
+    "revision": "121838e54cfd620dd723",
+    "url": "/dashboard-gestell/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bb47154979fc127f214e",
+    "url": "/dashboard-gestell/static/css/main.dcfb9000.chunk.css"
+  },
+  {
+    "revision": "121838e54cfd620dd723",
+    "url": "/dashboard-gestell/static/js/2.86897329.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dashboard-gestell/static/js/2.86897329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb47154979fc127f214e",
+    "url": "/dashboard-gestell/static/js/main.8165fb5c.chunk.js"
+  },
+  {
+    "revision": "62078114cda5a3368873",
+    "url": "/dashboard-gestell/static/js/runtime-main.89aae237.js"
+  },
+  {
+    "revision": "99b2f8b11c91bc893f24de36010eca36",
+    "url": "/dashboard-gestell/static/media/logo.99b2f8b1.svg"
+  }
+]);
